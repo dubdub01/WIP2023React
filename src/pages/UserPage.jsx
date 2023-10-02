3bfa26b04c5c863383766f370a3c5f106f2b37b1
duboismax <duboismax01@gmail.com>
2023-10-02 14:02:20 +0200

    user page start

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import UserApi from '../services/UserApi';
+import React, { useEffect, useState } from "react";
+import UserApi from "../services/UserApi";
 
 const UserPage = () => {
   const [user, setUser] = useState(null);
@@ -9,13 +9,16 @@ const UserPage = () => {
     UserApi.find()
       .then((response) => {
         // Accédez aux données de l'utilisateur dans la réponse
-        const userData = response['hydra:member'];
-        console.log(userData[0].company)
+        const userData = response["hydra:member"];
+        console.log(userData[0].company);
         // Mettez à jour l'état de l'utilisateur avec les données
         setUser(userData[0]);
       })
       .catch((error) => {
-        console.error('Erreur lors de la récupération des informations de l\'utilisateur :', error);
+        console.error(
+          "Erreur lors de la récupération des informations de l'utilisateur :",
+          error
+        );
       });
   }, []);
 
@@ -29,6 +32,27 @@ const UserPage = () => {
       <p>Nom d'utilisateur : {user.username}</p>
       <p>Adresse e-mail : {user.eMail}</p>
       {/* Affichez d'autres informations de l'utilisateur ici */}
+
+      <h2>Entreprises</h2>
+      <ul>
+        {user.company.map((company, index) => (
+          <div key={index}>
+            <p>
+              {company.name}
+            </p>
+          </div>
+        ))}
+      </ul>
+
+      <h2>Travailleurs associés</h2>
+      <ul>
+        {user.workers.map((worker, index) => (
+          <div key={index}>
+            <p>{worker.firstname}</p>
+            <p>{worker.lastname}</p>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 };
